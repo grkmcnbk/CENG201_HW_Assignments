dac17d2ad900c63f811cf04ce2d5fc9384efc091
Mahmut Görkem Canbek <gorkemcanbek715@gmail.com>
2025-12-19 14:04:47 +0300

    Add files via upload

---
+++ src/Patient.java
@@ -0,0 +1,19 @@
+public class Patient {
+
+    Patient next;
+
+    int id;
+    String name;
+    int severity;
+    int age;
+
+
+    Patient(){
+        id = 0;
+        name = null;
+        severity = 0;
+        age = 0;
+        next = null;
+    }
+
+}
